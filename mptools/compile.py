567fdb7bac2ec8a7972532330658e01e5d79ac48
Gordon McCreight <gordon@mccreight.com>
2019-07-27 20:34:23 -0700

    Add a utility that automatically adds guids to raw files

--- mptools/compile.py
+++ mptools/compile.py
@@ -2,19 +2,16 @@ import json
 from pathlib import Path
 from mptools.raw_to_notes import file_to_notes
 from mptools.notes_into_final import add_notes_to_template
+from .raw_tools import json_files
 
 
 def compile_to_final_crowdanki_json_in_project_root():
     all_notes = []
-    for f in raw_json_files():
+    for f in json_files():
         f = str(f)
         all_notes.extend(file_to_notes(f))
     json_block = all_notes_to_json_block(all_notes)
     add_notes_to_template(json_block)
-
-
-def raw_json_files():
-    return (Path(__file__).parent.parent / "data/raw").rglob("*.json")
 
 
 def all_notes_to_json_block(notes):
---
+++ mptools/raw_tools.py
@@ -0,0 +1,37 @@
+import json
+import copy
+from string import Template
+from pathlib import Path
+import random
+import re
+import logging
+
+# I really wanted to just copy some of Anki's code for generating a guid here,
+# but our license (BSD) is more permissive than Anki's (AGPL) so I'm just going
+# to throw some random characters together.  I realize this is tricky, because
+# it needs to be a clean-room implementation, which is a shame, because I bet
+# the Anki code is pretty good!
+
+
+def fill_empty_guids():
+    for f in json_files():
+        logging.debug(f"checking the guids in {f}")
+        d = f.read_text()
+        before = d
+        d = re.sub(r'"guid": "",', '"guid": "{}",'.format(guid()), d)
+        after = d
+        f.write_text(d)
+        if after != before:
+            logging.debug(f"added one or more guids to {f}")
+
+
+def guid():
+    f = []
+    for i in ((48, 10), (97, 26), (65, 26)):
+        f.extend([chr(i) for i in range(i[0], i[0] + i[1])])
+    f = f * 10
+    return "".join(random.sample(f, 10))
+
+
+def json_files():
+    return (Path(__file__).parent.parent / "data/raw").rglob("*.json")
